567dd050874f79de2a71c1da30eee5099d64c4af
Jeffrey McFarland <jlmcfarlandj@gmail.com>
2020-05-06 00:08:22 -0500

    changed styles for listed items

--- controllers/cocktail_controller.js
+++ controllers/cocktail_controller.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
+// Import the model to use its database functions.
 const cocktail = require("../models/cocktail.js");
 
 // Create all our routes and set up logic within those routes where required.
